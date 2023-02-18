ae093f009472d44b436bbeeed043776a6ca6810c
soumayabensassi <61428630+soumayabensassi@users.noreply.github.com>
2023-02-18 17:11:25 +0100

    firstcommit

---
+++ MicroGrowth/src/main/java/com/example/microgrowth/MicroGrowthApplication.java
@@ -0,0 +1,13 @@
+package com.example.microgrowth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicroGrowthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicroGrowthApplication.class, args);
+    }
+
+}
---
+++ MicroGrowth/src/test/java/com/example/microgrowth/MicroGrowthApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.microgrowth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MicroGrowthApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
