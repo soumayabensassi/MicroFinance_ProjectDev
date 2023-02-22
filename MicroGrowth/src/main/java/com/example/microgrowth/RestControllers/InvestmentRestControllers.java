00f0e887cd9f0522d84a7f90c59dd9eae719a2c1
Mariemomezzine <92644899+Mariemomezzine@users.noreply.github.com>
2023-02-22 12:19:39 +0100

    0

---
+++ MicroGrowth/src/main/java/com/example/microgrowth/RestControllers/InvestmentRestControllers.java
@@ -0,0 +1,30 @@
+package com.example.microgrowth.RestControllers;
+
+import com.example.microgrowth.DAO.Entities.Investment;
+import com.example.microgrowth.Service.Interfaces.IInvestment;
+import lombok.AllArgsConstructor;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@AllArgsConstructor
+public class InvestmentRestControllers  {
+    private IInvestment IInvestment;
+    @GetMapping("/afficher")
+    public List<Investment> afficher (){
+        return IInvestment.selectAll();
+    }
+    @PostMapping("/ajouterInvestment")
+    public Investment ajouter(@RequestBody Investment inv){
+
+        return IInvestment.add(inv);
+    }
+    @DeleteMapping("/deleteInvestmentbyID/{id}")
+    public void delete(@PathVariable int id)
+    {
+        IInvestment.deleteById(id);
+    }
+
+}
