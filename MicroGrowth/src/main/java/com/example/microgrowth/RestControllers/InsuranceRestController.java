1acd5d97525f3a23036f6347a60f83351e048c55
Aspex <marouani.ghaith@esprit.tn>
2023-03-07 11:59:22 +0100

    FonctionAvancées

--- MicroGrowth/src/main/java/com/example/microgrowth/RestControllers/InsuranceRestController.java
+++ MicroGrowth/src/main/java/com/example/microgrowth/RestControllers/InsuranceRestController.java
@@ -1,48 +1,107 @@
 package com.example.microgrowth.RestControllers;
 
 import com.example.microgrowth.DAO.Entities.ActivitySector;
+import com.example.microgrowth.DAO.Entities.Credit;
 import com.example.microgrowth.DAO.Entities.Inssurance;
+import com.example.microgrowth.DAO.Entities.User;
 import com.example.microgrowth.DAO.Repositories.InsuranceRepository;
+import com.example.microgrowth.DAO.Repositories.UserRepository;
+import com.example.microgrowth.Service.Interfaces.ICredit;
 import com.example.microgrowth.Service.Interfaces.IInsuranceService;
 import lombok.AllArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.time.Duration;
+import java.time.LocalDate;
+import java.time.temporal.Temporal;
+import java.util.Date;
 import java.util.List;
 
 
 
 @RestController
 @AllArgsConstructor
+@RequestMapping("/api/inssurances")
 
 public class InsuranceRestController {
     private IInsuranceService iInsuranceService;
-    @GetMapping("/afficherinsurance")
+    @GetMapping("/afficher-insurance")
     public List<Inssurance> afficher(){
         return iInsuranceService.selectAll();
 
     }
-    @PostMapping("/ajouterInsurance")
+    @PostMapping("/ajouter-Insurance")
     public Inssurance ajouter(@RequestBody Inssurance insurance){
         return  iInsuranceService.add(insurance);
 
     }
-    @PutMapping("/updateInsurance")
+    @PutMapping("/update-Insurance")
     public Inssurance update(@RequestBody Inssurance inssurance) {
         return iInsuranceService.edit(inssurance);
     }
-    @GetMapping("/afficherAvecIdInsurance/{id}")
+    @GetMapping("/afficherAvecIdInsurance/{idInsurance}")
 
     public Inssurance afficherAvecIdInsurance(@PathVariable int idInsurance) {
         return iInsuranceService.selectById(idInsurance);
     }
-    @DeleteMapping("/deleteInsurance/{id}")
+    @DeleteMapping("/deleteInsurance/{idInsurance}")
     public void delete(@PathVariable int idInsurance)
     {
 
         iInsuranceService.deleteById(idInsurance);
     }
 
+
+    private InsuranceRepository insuranceRepository;
+
+
+
+    @PostMapping("/calculate-monthly-payment")
+    public ResponseEntity<Double> calculateMonthlyPayment(@RequestParam Double loanAmount) {
+
+        Double interestRate = 0.12;
+        Double monthlyInterestRate = interestRate / 12;
+        Long loanTerm = (Inssurance.getDuration())/30;
+        Double monthlyPaymentAmount = (loanAmount * monthlyInterestRate) / (1 - Math.pow(1 + monthlyInterestRate, -12 * loanTerm));
+
+        // return the monthly payment amount
+        return new ResponseEntity<>(monthlyPaymentAmount, HttpStatus.OK);
+    }
+
+
+    private ICredit iCredit;
+
+    @PostMapping("/insurance-request")
+    public String applyForInsurance(@RequestParam  float income, @RequestParam String mail, @RequestParam int Score) {
+
+
+
+        List<Credit> lcredit=iCredit.SelectByEmail(mail);
+        float S=0;
+        for (Credit l : lcredit) {
+            S+=l.getAmount();
+        }
+        System.out.println(S);
+        double dti = calculateDTI(income, S);
+        int creditScore = Score;
+
+        if (dti > 0.43 || creditScore < 600) {
+            return "Sorry, your insurance request has been refused.";
+        } else {
+            return "Congratulations, your insurance request has been accepted.";
+        }
+    }
+
+    private double calculateDTI(double income, double debt) {
+        return debt / income;
+    }
+
+
 }
+
 
 
 
