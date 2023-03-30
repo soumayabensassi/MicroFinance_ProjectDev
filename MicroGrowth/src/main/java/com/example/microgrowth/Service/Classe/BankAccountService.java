573a05d65d452d17ec4e630f9ffd3b778dc0ce45
momed2809 <92689181+momed2809@users.noreply.github.com>
2023-03-30 15:05:51 +0100

    Limit scheduler

--- MicroGrowth/src/main/java/com/example/microgrowth/Service/Classe/BankAccountService.java
+++ MicroGrowth/src/main/java/com/example/microgrowth/Service/Classe/BankAccountService.java
@@ -4,6 +4,7 @@ import com.example.microgrowth.DAO.Entities.BankAccount;
 import com.example.microgrowth.DAO.Repositories.BankAccountRepository;
 import com.example.microgrowth.Service.Interfaces.IBankAccount;
 import lombok.AllArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
@@ -12,32 +13,56 @@ import java.util.List;
 public class BankAccountService implements IBankAccount {
 
 
+    private BankAccountRepository bankAccountRepository;
 
 
-        private BankAccountRepository bankAccountRepository;
-        @Override
-        public BankAccount add(BankAccount a) {
-            return bankAccountRepository.save(a);
+    @Override
+    public BankAccount add(BankAccount a) {
+        return bankAccountRepository.save(a);
+    }
+
+    @Override
+    public BankAccount edit(BankAccount a) {
+        return bankAccountRepository.save(a);
+    }
+
+    @Override
+    public List<BankAccount> selectAll() {
+        return bankAccountRepository.findAll();
+    }
+
+    @Override
+    public BankAccount SelectById(int id) {
+        return bankAccountRepository.findById(id).get();
+    }
+
+    @Override
+    public void deleteById(int id) {
+        bankAccountRepository.deleteById(id);
+
+    }
+
+    @Override
+    @Scheduled(cron = "0 0 0 * * * ")
+    public void resetLimit() {
+        for (BankAccount bankAccount : bankAccountRepository.findAll()) {
+            bankAccount.setCURRENT_BANKTRANSFER_PER_DAY(bankAccount.getMAX_BANKTRANSFER_PER_DAY());
+            bankAccount.setCURRENT_CARDPAYMENT_PER_DAY(bankAccount.getMAX_CARDPAYMENT_PER_DAY());
+            bankAccount.setCURRENT_WITHDRAWAL_PER_DAY(bankAccount.getMAX_CARDPAYMENT_PER_DAY());
+
+        }
+    }
+
+  /*  @Override
+    @Scheduled(cron = "0 0 1 * * ")
+    public void notifUpdate() {
+        for (BankAccount bankAccount : bankAccountRepository.findAll()) {
+           // bankAccount.getIdBank()
+
+
+
+
         }
 
-        @Override
-        public BankAccount edit(BankAccount a) {
-            return bankAccountRepository.save(a);
-        }
-
-        @Override
-        public List<BankAccount> selectAll() {
-            return bankAccountRepository.findAll();
-        }
-
-        @Override
-        public BankAccount SelectById(int id) {
-            return bankAccountRepository.findById(id).get();
-        }
-
-        @Override
-        public void deleteById(int id) {
-            bankAccountRepository.deleteById(id);
-
-        }
+    }*/
 }
--- MicroGrowth/src/main/java/com/example/microgrowth/Service/Interfaces/IBankAccount.java
+++ MicroGrowth/src/main/java/com/example/microgrowth/Service/Interfaces/IBankAccount.java
@@ -13,5 +13,8 @@ public interface IBankAccount {
     List<BankAccount> selectAll();
     BankAccount SelectById(int id);
     void deleteById(int id);
+    void resetLimit();
+
+    //void notifUpdate();
 
 }
