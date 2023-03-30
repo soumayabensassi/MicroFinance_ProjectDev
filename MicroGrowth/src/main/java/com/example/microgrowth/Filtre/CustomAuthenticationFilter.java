18329e29caa45f23177d42f1d44e5320ec3265b0
soumayabensassi <61428630+soumayabensassi@users.noreply.github.com>
2023-03-30 22:31:59 +0100

    update

--- MicroGrowth/src/main/java/com/example/microgrowth/Filtre/CustomAuthenticationFilter.java
+++ MicroGrowth/src/main/java/com/example/microgrowth/Filtre/CustomAuthenticationFilter.java
@@ -48,7 +48,7 @@ public class CustomAuthenticationFilter extends UsernamePasswordAuthenticationFi
         Algorithm algorithm = Algorithm.HMAC256("secret".getBytes());
         String access_token = JWT.create()
                 .withSubject(user.getUsername())
-                .withExpiresAt(new Date(System.currentTimeMillis() +15*60*1000))
+                .withExpiresAt(new Date(System.currentTimeMillis() +60*60*1000))
                 .withIssuer(request.getRequestURL().toString())
                 .withClaim("roles",user.getAuthorities().stream().map(GrantedAuthority::getAuthority).collect(Collectors.toList()))
                 .sign(algorithm);
